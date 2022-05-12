3b1ae8d250ae944ea7355e2dc4a5c5d6797578d6
lucasferreira94 <lucas_sferreira94@hotmail.com>
2022-05-12 08:53:56 -0300

    atualizacao aula de lambda

--- Python_Intermediario_Programacao_Procedural/aula2 Funcoes_Lambda/expressoes_lambda.py
+++ Python_Intermediario_Programacao_Procedural/aula2 Funcoes_Lambda/expressoes_lambda.py
@@ -25,8 +25,28 @@ def func(item):
     return item[1]
 
 
-#lista.sort(key=func)  # Imprimir do mais barato ao mais caro
-#lista.sort(key=lambda item: item[1])
+# lista.sort(key=func)  # Imprimir do mais barato ao mais caro
+# lista.sort(key=lambda item: item[1])
 #lista.sort(key=lambda item: item[1], reverse=True)  # imprimir na ordem invertida
-#print(lista)
-print(sorted(lista, key=lambda i: i[1]))
+# print(lista)
+# print(sorted(lista, key=lambda i: i[1]))
+
+"""
+# EXEMPLO 2 --> calcular o imposto de 30%
+# EM UMA FUNÇÃO NORMAL:
+preco = 1000
+
+def calcular_imposto(preco):
+    return preco * 0.3
+
+print(calcular_imposto(preco))
+
+# EM FORMATO LAMBDA
+calcular_imposto2 = lambda preco: preco * 0.3  # Antes do ":" é valor que será passado ao LAMBDA. O que vier depois do ":" é a resposta
+print(calcular_imposto2(preco))
+"""
+
+# EXEMPLO 3
+precos = [100, 500, 10, 25]
+impostos = list(map(lambda x: x*0.3, precos))
+print(impostos)
